aa79aef04b4d251b142934803c456f79b3607383
Arsen <arsenkerezbekov7@gmail.com>
2024-11-27 22:36:17 +0600

    feat: create-stucture app

---
+++ src/context/Client/ClientContext.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ClientHeader = () => {
+    return (
+        <div>
+            <h1>ClientContext</h1>
+        </div>
+    );
+};
+
+export default ClientHeader;
---
+++ src/context/admin/ContextAdmin.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContextAdmin = () => {
+    return (
+        <div>
+            <h1>ContextAdmin</h1>
+        </div>
+    );
+};
+
+export default ContextAdmin;
