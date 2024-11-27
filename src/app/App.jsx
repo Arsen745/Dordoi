0970513d934fe41557fc69e6ccb2107c209411fb
Almir905 <almirkerezbekov11@gmail.com>
2024-11-27 23:53:00 +0600

    about-page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const App = () => {
     return (
         <div>
-            <h1>Master</h1>
-            <h1>Dev</h1>
+            <h1>Главный Страница</h1>
         </div>
     );
 };
