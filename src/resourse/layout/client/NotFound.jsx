b8c153ff9599fb8d3a645bbeff2de5dc378f8966
Arsen <arsenkerezbekov7@gmail.com>
2024-12-01 15:39:55 +0600

    feat: page-cart

---
+++ src/resourse/layout/client/NotFound.jsx
@@ -0,0 +1,11 @@
+
+const NotFound = () => {
+    return (
+        <div className='container'>
+            <h1>404 - страница не найдено</h1>
+            <a href="/">Вернутся главную</a>
+        </div>
+    );
+};
+
+export default NotFound;
